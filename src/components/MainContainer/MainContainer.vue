46cb2abff9ca90f632df229b48b32e432e382d36
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-02-20 05:32:01 +0000

    Sending an Array

--- src/components/MainContainer/MainContainer.vue
+++ src/components/MainContainer/MainContainer.vue
@@ -1,16 +1,20 @@
 <template>
     <div class="MainContainer">
         <Title></Title>
+        <TodoList v-bind:todo_list="['item 1','item 2', 'item 3']" />
     </div>
     
 </template>
 
 <script>
 import Title from '../ContainerTitle/ContainerTitle.vue';
+import TodoList from '../TodoList/TodoList.vue';
 export default
 {
+    name:'MainContainer',
     components:{
-        Title
+        Title,
+        TodoList
     }
 }
 </script>
---
+++ src/components/TodoList/TodoList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="List">
+        {{ todo_list }}
+    </div>
+</template>
+
+<script>
+export default
+{
+    name:'TodoList',
+    props:
+    {
+        todo_list:Array
+    }
+}
+</script>
